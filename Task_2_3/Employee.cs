9f6e331097c779308187e54b39a443f639c1a8c6
Jon Miller <jordjik.don@gmail.com>
2023-02-12 19:15:45 +0300

    Add project files.

---
+++ Task_2_3/Employee.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2_3
+{
+    class Employee
+    {
+        string name, surname;
+        public Employee(string name,string surname)
+        {
+            this.name = name;
+            this.surname = surname;
+        }
+
+        public decimal Post_assistant()
+        {
+            return;
+        }
+
+        public decimal Post_specialist()
+        {
+            return;
+        }
+
+        public decimal Post_manager()
+        {
+            return;
+        }
+    }
+}
---
+++ Task_2_3/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2_3
+{
+    class Program
+    {
+        /*
+         Задание 3 
+         Используя Visual Studio, создайте проект по шаблону Console Application. 
+         Требуется: 
+         1-Создать класс Employee. 
+         2-В теле класса создать пользовательский конструктор, который принимает два строковых аргумента, и 
+           инициализирует поля, соответствующие фамилии и имени сотрудника. 
+         3-Создать метод рассчитывающий оклад сотрудника (в зависимости от должности и стажа) и налоговый сбор. 
+         4-Написать программу, которая выводит на экран информацию о сотруднике (фамилия, имя, должность), 
+           оклад и налоговый сбор.
+         */
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите имя сотрудника...");
+            string name = Convert.ToString(Console.ReadLine());
+
+            Console.WriteLine("Введите фамилию сотрудника...");
+            string surname = Convert.ToString(Console.ReadLine());
+
+            Console.WriteLine("Введите должность сотрудника:1-помощник,2-специалист,3-менеджер");
+            int post = Convert.ToInt32(Console.ReadLine());
+            switch (post)
+            {
+                case 1:
+                    Post_assistant();
+                    break;
+                case 2:
+                    Post_specialist();
+                    break;
+                case 3:
+                    Post_manager();
+                    break;
+                default:
+                    break;
+            }
+
+
+            Employee employee = new Employee(name,surname);
+
+            Console.ReadKey();
+        }
+    }
+}
